e7636b4d6afd977bb878b585292c60bef4e504c5
Fahimansari <fahimpjan@gmail.com>
2023-05-17 16:32:14 +0530

    AWS Region value is also migrated to .env file

--- server/src/functions/getLink.ts
+++ server/src/functions/getLink.ts
@@ -1,12 +1,16 @@
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import * as dotenv from "dotenv";
 
+dotenv.config()
+
+const awsRegion = process.env.AWS_REGION
 
 
 async function getLink() {
     const bucketName = "samp-bucket-test2";
     const fileName = "abhijith.jpg";
 
-    return `https://${bucketName}.s3.ap-south-1.amazonaws.com/${fileName}`
+    return `https://${bucketName}.s3.${awsRegion}.amazonaws.com/${fileName}`
 
 }
 
