a3cc068b980ced357196b47b8319e85ae3212717
Fahimansari <fahimpjan@gmail.com>
2023-05-24 13:13:53 +0530

    The chances of getting an unhadled error are low. Need to make it more user-friendly

--- client/components/ImageUploader.tsx
+++ client/components/ImageUploader.tsx
@@ -31,7 +31,7 @@ export default function ImageUploader({ onUpload, selectedFile, setSelectedFile,
           className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
         >
           <span >Upload a file</span>
-          <input id="file-upload" name="file-upload" accept="image/jpeg" type="file" onChange={handleFileChange} ref={fileInputRef} className="sr-only" />
+          <input id="file-upload" name="file-upload" accept="image/jpeg" type="file" onChange={handleFileChange} ref={fileInputRef} className="sr-only"  required />
         </label>
         <p className="pl-1">or drag and drop</p>
       </div>
